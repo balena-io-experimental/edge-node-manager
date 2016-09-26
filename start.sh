7c4f3c3cf67c81ccbdefd83301d03adbcaf8803b
Joseph Roberts <j.baldwin.roberts@gmail.com>
2016-09-26 12:18:19 +0100

    Initialised repo

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Enable on-board bluetooth
 ./enableBle.sh
+
+# Compile and run the edge-node-manager
+# The code is recompiled everytime the container restarts which is not ideal but OK for now
 godep go run main.go
