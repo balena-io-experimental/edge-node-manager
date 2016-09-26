f925c44d5a998cc9f52e7de12157f1112b6f4742
Joseph Roberts <j.baldwin.roberts@gmail.com>
2016-09-26 12:34:45 +0100

    Initialised repo

--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@
 
 # Compile and run the edge-node-manager
 # The code is recompiled everytime the container restarts which is not ideal but OK for now
+echo "Starting edge-node-manager..."
 godep go run main.go
