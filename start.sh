8f3be85fe2ae967d0d255ac0d93c5080ba22470f
Joseph Roberts <j.baldwin.roberts@gmail.com>
2016-09-27 15:44:30 +0100

    Refactored build method slightly

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 # Enable on-board bluetooth
 ./enableBle.sh
 
-# Compile and run the edge-node-manager
-# The code is recompiled everytime the container restarts which is not ideal but OK for now
+# Run the edge-node-manager
 echo "Starting edge-node-manager..."
-godep go run main.go
+./edge-node-manager
