893ee9b06df45aa7f14412723d279785f971a0a7
Joseph Roberts <j.baldwin.roberts@gmail.com>
2016-10-25 15:34:56 +0100

    Microbit (#40)

---
+++ board/nrf51822dk/nrf51822dk.go
@@ -0,0 +1,160 @@
+package nrf51822dk
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/josephroberts/edge-node-manager/micro/nrf51822"
+	"github.com/josephroberts/edge-node-manager/radio/bluetooth"
+	"github.com/paypal/gatt"
+)
+
+type Nrf51822dk struct {
+	Micro nrf51822.Nrf51822
+}
+
+func (b Nrf51822dk) Update(path string) error {
+	if err := b.Micro.ExtractFirmware(path, "nrf51422_xxac_s130.bin", "nrf51422_xxac_s130.dat"); err != nil {
+		return err
+	}
+
+	bluetooth.Radio.Handle(
+		gatt.PeripheralDiscovered(b.Micro.OnPeriphDiscovered),
+		gatt.PeripheralConnected(b.bootloadOnPeriphConnected),
+		gatt.PeripheralDisconnected(b.Micro.OnPeriphDisconnected),
+	)
+	if err := bluetooth.Radio.Init(bluetooth.OnStateChanged); err != nil {
+		return err
+	}
+
+	var savedErr error
+	var savedRestart bool
+	for {
+		select {
+		case <-time.After(120 * time.Second):
+			return fmt.Errorf("Update timed out")
+		case savedErr = <-b.Micro.ErrChannel:
+		case savedRestart = <-b.Micro.RestartChannel:
+		case connected := <-b.Micro.ConnectedChannel:
+			if connected {
+				log.Debug("Connected")
+			} else {
+				log.Debug("Disconnected")
+
+				if !savedRestart {
+					return savedErr
+				}
+
+				savedRestart = false
+
+				// Time delay to allow device to restart after being placed into bootloader mode
+				time.Sleep(1 * time.Second)
+
+				bluetooth.Radio.Handle(
+					gatt.PeripheralDiscovered(b.Micro.OnPeriphDiscovered),
+					gatt.PeripheralConnected(b.Micro.UpdateOnPeriphConnected),
+					gatt.PeripheralDisconnected(b.Micro.OnPeriphDisconnected),
+				)
+				if err := bluetooth.Radio.Init(bluetooth.OnStateChanged); err != nil {
+					return err
+				}
+			}
+		}
+	}
+}
+
+func (b Nrf51822dk) Scan(applicationUUID int) (map[string]bool, error) {
+	return bluetooth.Scan(strconv.Itoa(applicationUUID), 10)
+}
+
+func (b Nrf51822dk) Online() (bool, error) {
+	return bluetooth.Online(b.Micro.LocalUUID, 10)
+}
+
+func (b Nrf51822dk) Restart() error {
+	return b.Micro.ProcessRequest(b.restartOnPeriphConnected)
+}
+
+func (b Nrf51822dk) Identify() error {
+	return b.Micro.ProcessRequest(b.identifyOnPeriphConnected)
+}
+
+func (b Nrf51822dk) bootloadOnPeriphConnected(periph gatt.Peripheral, err error) {
+	defer periph.Device().CancelConnection(periph)
+
+	b.Micro.ConnectedChannel <- true
+
+	if err := periph.SetMTU(500); err != nil {
+		b.Micro.ErrChannel <- err
+		return
+	}
+
+	name, err := bluetooth.GetName(periph)
+	if err != nil {
+		b.Micro.ErrChannel <- err
+		return
+	}
+
+	if name == "DfuTarg" {
+		log.Debug("Bootloader started")
+		b.Micro.UpdateOnPeriphConnected(periph, err)
+		return
+	}
+
+	log.Debug("Bootloader not started")
+	log.Debug("Starting bootloader")
+
+	b.Micro.RestartChannel <- true
+
+	if err := b.Micro.EnableCCCD(periph); err != nil {
+		b.Micro.ErrChannel <- err
+		return
+	}
+
+	if err := b.Micro.WriteDFUControlPoint(periph, []byte{nrf51822.Start, 0x04}, false); err != nil {
+		b.Micro.ErrChannel <- err
+		return
+	}
+
+	log.Debug("Started bootloader")
+}
+
+func (b Nrf51822dk) restartOnPeriphConnected(periph gatt.Peripheral, err error) {
+	defer periph.Device().CancelConnection(periph)
+
+	b.Micro.ConnectedChannel <- true
+
+	characteristic, err := bluetooth.GetChar("0000f00d1212efde1523785fef13d123", "0000feed1212efde1523785fef13d123", "", gatt.CharWrite, 23, 24)
+	if err != nil {
+		b.Micro.ErrChannel <- err
+		return
+	}
+
+	if err = periph.WriteCharacteristic(characteristic, []byte{0x01}, false); err != nil {
+		b.Micro.ErrChannel <- err
+		return
+	}
+
+	return
+}
+
+func (b Nrf51822dk) identifyOnPeriphConnected(periph gatt.Peripheral, err error) {
+	defer periph.Device().CancelConnection(periph)
+
+	b.Micro.ConnectedChannel <- true
+
+	characteristic, err := bluetooth.GetChar("0000f00d1212efde1523785fef13d123", "0000beef1212efde1523785fef13d123", "", gatt.CharWrite, 21, 22)
+	if err != nil {
+		b.Micro.ErrChannel <- err
+		return
+	}
+
+	if err = periph.WriteCharacteristic(characteristic, []byte{0x01}, false); err != nil {
+		b.Micro.ErrChannel <- err
+		return
+	}
+
+	return
+}
--- device/device.go
+++ device/device.go
@@ -4,73 +4,48 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/josephroberts/edge-node-manager/board"
 	"github.com/josephroberts/edge-node-manager/database"
-	"github.com/josephroberts/edge-node-manager/micro"
+	"github.com/josephroberts/edge-node-manager/device/status"
 	"github.com/josephroberts/edge-node-manager/supervisor"
 )
 
-// Type contains the micro that make up a device type
-type Type struct {
-	Micro micro.Type `json:"micro"`
-}
-
-// Status defines the device statuses
-type Status string
-
-const (
-	DOWNLOADING Status = "Downloading"
-	INSTALLING         = "Installing"
-	STARTING           = "Starting"
-	STOPPING           = "Stopping"
-	IDLE               = "Idle"
-	OFFLINE            = "Offline"
-)
-
-// Device contains all the variables needed to define a device
 type Device struct {
-	Type            `json:"type"`
-	LocalUUID       string      `json:"localUUID"`
-	UUID            string      `json:"uuid"`
-	Name            string      `json:"name"`
-	ApplicationUUID int         `json:"applicationUUID"`
-	ApplicationName string      `json:"applicationName"`
-	Commit          string      `json:"commit"`
-	TargetCommit    string      `json:"targetCommit"`
-	Status          Status      `json:"status"`
-	Progress        float32     `json:"progress"`
-	RestartFlag     bool        `json:"restartFlag"`
-	Config          interface{} `json:"config"`
-	Environment     interface{} `json:"environment"`
-}
-
-// Interface defines the common functions a device must implement
-type Interface interface {
-	String() string
-	Update(path string) error
-	Scan() (map[string]bool, error)
-	Online() (bool, error)
-	Restart() error
+	Board           board.Interface `json:"-"`
+	Name            string          `json:"name"`
+	BoardType       board.Type      `json:"boardType"`
+	LocalUUID       string          `json:"localUUID"`
+	ResinUUID       string          `json:"resinUUID"`
+	ApplicationUUID int             `json:"applicationUUID"`
+	ApplicationName string          `json:"applicationName"`
+	Commit          string          `json:"commit"`
+	TargetCommit    string          `json:"targetCommit"`
+	Status          status.Status   `json:"status"`
+	Progress        float32         `json:"progress"`
+	RestartFlag     bool            `json:"restartFlag"`
+	Config          interface{}     `json:"config"`
+	Environment     interface{}     `json:"environment"`
 }
 
 func (d Device) String() string {
 	return fmt.Sprintf(
-		"Micro type: %s, "+
+		"Name: %s, "+
+			"Board type: %s, "+
 			"Local UUID: %s, "+
-			"UUID: %s, "+
-			"Name: %s, "+
+			"Resin UUID: %s, "+
 			"Application UUID: %d, "+
 			"Application name: %s, "+
 			"Commit: %s, "+
 			"Target commit: %s, "+
 			"Status: %s, "+
-			"Progress: %2.2f "+
+			"Progress: %2.2f, "+
 			"Restart flag: %t, "+
 			"Config: %v, "+
 			"Environment: %v",
-		d.Type.Micro,
-		d.LocalUUID,
-		d.UUID,
 		d.Name,
+		d.BoardType,
+		d.LocalUUID,
+		d.ResinUUID,
 		d.ApplicationUUID,
 		d.ApplicationName,
 		d.Commit,
@@ -82,101 +57,67 @@ func (d Device) String() string {
 		d.Environment)
 }
 
-// Update updates a specific device
-func (d Device) Update(path string) error {
-	err := d.Cast().Update(path)
-	return err
-}
-
-// Scan checks which devices are online
-func (d Device) Scan() (map[string]bool, error) {
-	return d.Cast().Scan()
-}
-
-// Online checks if a specific device is online
-func (d Device) Online() (bool, error) {
-	return d.Cast().Online()
-}
-
-// Restart restarts a specific device
-func (d Device) Restart() error {
-	return d.Cast().Restart()
-}
-
-// New creates a new device and puts it into the database
-func New(deviceType Type, localUUID, UUID, name string, applicationUUID int, applicationName, targetCommit string, config, environment interface{}) error {
-	newDevice := &Device{
-		Type:            deviceType,
-		LocalUUID:       localUUID,
-		UUID:            UUID,
+func Create(boardType board.Type, name, localUUID, resinUUID string, applicationUUID int, applicationName, targetCommit string, config, environment interface{}) (*Device, error) {
+	d := &Device{
+		Board:           board.Create(boardType, localUUID),
 		Name:            name,
+		BoardType:       boardType,
+		LocalUUID:       localUUID,
+		ResinUUID:       resinUUID,
 		ApplicationUUID: applicationUUID,
 		ApplicationName: applicationName,
 		Commit:          "",
 		TargetCommit:    targetCommit,
-		Status:          OFFLINE,
+		Status:          status.OFFLINE,
 		Progress:        0.0,
 		RestartFlag:     false,
 		Config:          config,
 		Environment:     environment,
 	}
 
-	buffer, err := json.Marshal(newDevice)
+	return d, d.putDevice()
+}
+
+func (d *Device) putDevice() error {
+	bytes, err := d.Marshall()
 	if err != nil {
 		return err
 	}
 
-	return database.PutDevice(newDevice.ApplicationUUID, newDevice.LocalUUID, newDevice.UUID, buffer)
+	return database.PutDevice(d.ApplicationUUID, d.LocalUUID, d.ResinUUID, bytes)
 }
 
-// Get gets a single device for a specific application
-func Get(applicationUUID int, UUID string) (*Device, error) {
-	buffer, err := database.GetDevice(applicationUUID, UUID)
-	if err != nil {
+func (d *Device) Marshall() ([]byte, error) {
+	return json.Marshal(d)
+}
+
+func Unmarshall(bytes []byte) (*Device, error) {
+	var d *Device
+	if err := json.Unmarshal(bytes, &d); err != nil {
 		return nil, err
 	}
 
-	var device Device
-	if err = json.Unmarshal(buffer, &device); err != nil {
-		return nil, err
-	}
+	d.Board = board.Create(d.BoardType, d.LocalUUID)
 
-	return &device, nil
+	return d, nil
 }
 
-// Cast converts the base device type to its actual type
-func (d Device) Cast() Interface {
-	switch d.Type.Micro {
-	case micro.NRF51822:
-		return (Nrf51822)(d)
-	case micro.ESP8266:
-		return (Esp8266)(d)
-	}
-
-	return nil
-}
-
-// SetStatus sets the state for a specific device
 // Only set the is_online field if the device is_online state has changed
-func (d *Device) SetStatus(status Status) []error {
-	// Get the old is_online state
-	old := true
-	if d.Status == OFFLINE {
-		old = false
+func (d *Device) SetStatus(newStatus status.Status) []error {
+	oldOnline := true
+	if d.Status == status.OFFLINE {
+		oldOnline = false
 	}
 
-	d.Status = status
+	d.Status = newStatus
 
-	// Get the new is_online state
-	new := true
-	if d.Status == OFFLINE {
-		new = false
+	newOnline := true
+	if d.Status == status.OFFLINE {
+		newOnline = false
 	}
 
-	// Update the is_online state
-	if old != new {
-		return supervisor.DependantDeviceInfoUpdateWithOnlineState(d.UUID, (string)(d.Status), d.Commit, new)
+	if oldOnline != newOnline {
+		return supervisor.DependantDeviceInfoUpdateWithOnlineState(d.ResinUUID, (string)(d.Status), d.Commit, newOnline)
 	}
-	// Don't update the is_online state
-	return supervisor.DependantDeviceInfoUpdateWithoutOnlineState(d.UUID, (string)(d.Status), d.Commit)
+	return supervisor.DependantDeviceInfoUpdateWithoutOnlineState(d.ResinUUID, (string)(d.Status), d.Commit)
 }
