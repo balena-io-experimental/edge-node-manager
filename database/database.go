f33c2db92c6c9863a31f2daa224907cbb7cb7aea
Joseph Roberts <j.baldwin.roberts@gmail.com>
2016-11-02 17:38:09 +0000

    Writing tests for new DB functions

--- database/database.go
+++ database/database.go
@@ -1,51 +1,21 @@
 package database
 
 import (
-	"bytes"
-	"encoding/binary"
 	"fmt"
 	"os"
 	"path"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/boltdb/bolt"
-	"github.com/resin-io/edge-node-manager/config"
 )
 
 var dbPath string
 
-func PutDevice(applicationUUID int, localUUID, deviceUUID string, device []byte) error {
-	db, err := open()
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	if err = db.Update(func(tx *bolt.Tx) error {
-		var b *bolt.Bucket
-		if b = tx.Bucket([]byte("Applications")); b == nil {
-			return fmt.Errorf("Bucket not found")
-		}
-
-		var converted []byte
-		if converted, err = i2b(applicationUUID); err != nil {
-			return err
-		}
-
-		var a *bolt.Bucket
-		if a, err = b.CreateBucketIfNotExists(converted); err != nil {
-			return err
-		}
-
-		return a.Put([]byte(deviceUUID), device)
-	}); err != nil {
-		return err
-	}
-
-	return putDeviceMapping(db, applicationUUID, localUUID, deviceUUID)
-}
-
-func PutDevices(applicationUUID int, devices map[string][]byte) error {
+// Write a map to the database
+// p is the outer bucket key (applicationUUID)
+// c is the inner bucket key(resinUUID)
+// d is the key/value map
+func Write(p, c string, d map[string][]byte) error {
 	db, err := open()
 	if err != nil {
 		return err
@@ -53,23 +23,23 @@ func PutDevices(applicationUUID int, devices map[string][]byte) error {
 	defer db.Close()
 
 	return db.Update(func(tx *bolt.Tx) error {
-		var b *bolt.Bucket
-		if b = tx.Bucket([]byte("Applications")); b == nil {
-			return fmt.Errorf("Bucket not found")
-		}
-
-		var converted []byte
-		if converted, err = i2b(applicationUUID); err != nil {
+		root, err := tx.CreateBucketIfNotExists([]byte("applications"))
+		if err != nil {
 			return err
 		}
 
-		var a *bolt.Bucket
-		if a, err = b.CreateBucketIfNotExists(converted); err != nil {
+		parent, err := root.CreateBucketIfNotExists([]byte(p))
+		if err != nil {
 			return err
 		}
 
-		for key, value := range devices {
-			if err = a.Put([]byte(key), value); err != nil {
+		child, err := parent.CreateBucketIfNotExists([]byte(c))
+		if err != nil {
+			return err
+		}
+
+		for k, v := range d {
+			if err := child.Put([]byte(k), v); err != nil {
 				return err
 			}
 		}
@@ -78,267 +48,428 @@ func PutDevices(applicationUUID int, devices map[string][]byte) error {
 	})
 }
 
-func GetDevice(applicationUUID int, deviceUUID string) ([]byte, error) {
+// WriteField writes a field to the database
+// p is the outer bucket key (applicationUUID)
+// c is the inner bucket key(resinUUID)
+// k is the field key
+// v is the field value
+func WriteField(p, c, k string, v []byte) error {
 	db, err := open()
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer db.Close()
 
-	var device []byte
-	if err = db.View(func(tx *bolt.Tx) error {
-		var b *bolt.Bucket
-		if b = tx.Bucket([]byte("Applications")); b == nil {
-			return fmt.Errorf("Bucket not found")
-		}
-
-		var converted []byte
-		if converted, err = i2b(applicationUUID); err != nil {
+	return db.Update(func(tx *bolt.Tx) error {
+		root, err := tx.CreateBucketIfNotExists([]byte("applications"))
+		if err != nil {
 			return err
 		}
 
-		var a *bolt.Bucket
-		if a = b.Bucket(converted); a == nil {
-			return fmt.Errorf("Bucket not found")
+		parent, err := root.CreateBucketIfNotExists([]byte(p))
+		if err != nil {
+			return err
 		}
 
-		var value []byte
-		if value = a.Get([]byte(deviceUUID)); value == nil {
-			return fmt.Errorf("Value not found")
+		child, err := parent.CreateBucketIfNotExists([]byte(c))
+		if err != nil {
+			return err
 		}
 
-		device = make([]byte, len(value))
-		copy(device, value)
-
-		return nil
-	}); err != nil {
-		return nil, err
-	}
-
-	return device, nil
+		return child.Put([]byte(k), v)
+	})
 }
 
-func GetDevices(applicationUUID int) (map[string][]byte, error) {
+// Read a map from the database
+// p is the outer bucket key (applicationUUID)
+// c is the inner bucket key (resinUUID)
+// Returns map key/value
+func Read(p, c string) (map[string][]byte, error) {
 	db, err := open()
 	if err != nil {
 		return nil, err
 	}
 	defer db.Close()
 
-	var devices map[string][]byte
-	if err = db.View(func(tx *bolt.Tx) error {
-		var b *bolt.Bucket
-		if b = tx.Bucket([]byte("Applications")); b == nil {
-			return fmt.Errorf("Bucket not found")
-		}
+	d := make(map[string][]byte)
 
-		var converted []byte
-		if converted, err = i2b(applicationUUID); err != nil {
+	err = db.View(func(tx *bolt.Tx) error {
+		root := tx.Bucket([]byte("applications"))
+		if root == nil {
 			return err
 		}
 
-		var a *bolt.Bucket
-		if a = b.Bucket(converted); a == nil {
-			return nil
+		parent := root.Bucket([]byte(p))
+		if parent == nil {
+			return fmt.Errorf("Bucket %s not found", (string)(p))
 		}
 
-		devices = make(map[string][]byte)
-		return a.ForEach(func(k, v []byte) error {
-			key := make([]byte, len(k))
+		child := parent.Bucket([]byte(c))
+		if child == nil {
+			return fmt.Errorf("Bucket %s not found", (string)(c))
+		}
+
+		i := child.Cursor()
+		for k, v := i.First(); k != nil; k, v = i.Next() {
 			value := make([]byte, len(v))
-			copy(key, k)
 			copy(value, v)
-			devices[(string)(key)] = value
-			return nil
-		})
-	}); err != nil {
-		return nil, err
-	}
+			d[(string)(k)] = value
+		}
 
-	return devices, nil
+		return nil
+	})
+
+	return d, err
 }
 
-func DeleteDevice(applicationUUID int, deviceUUID string) error {
+// ReadField reads a field from the database
+// p is the outer bucket key (applicationUUID)
+// c is the inner bucket key (resinUUID)
+// k is the field key
+// Returns field value
+func ReadField(p, c, k string) ([]byte, error) {
 	db, err := open()
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer db.Close()
 
-	if err := db.Update(func(tx *bolt.Tx) error {
-		var b *bolt.Bucket
-		if b = tx.Bucket([]byte("Applications")); b == nil {
-			return fmt.Errorf("Bucket not found")
-		}
+	var d []byte
 
-		var converted []byte
-		if converted, err = i2b(applicationUUID); err != nil {
+	err = db.View(func(tx *bolt.Tx) error {
+		root := tx.Bucket([]byte("applications"))
+		if root == nil {
 			return err
 		}
 
-		var a *bolt.Bucket
-		if a = b.Bucket(converted); a == nil {
-			return nil
+		parent := root.Bucket([]byte(p))
+		if parent == nil {
+			return fmt.Errorf("Bucket %s not found", (string)(p))
 		}
 
-		return a.Delete([]byte(deviceUUID))
-	}); err != nil {
-		return err
-	}
+		child := parent.Bucket([]byte(c))
+		if child == nil {
+			return fmt.Errorf("Bucket %s not found", (string)(c))
+		}
 
-	return deleteDeviceMapping(db, deviceUUID)
+		v := child.Get([]byte(k))
+
+		value := make([]byte, len(v))
+		copy(value, v)
+		d = value
+
+		return nil
+	})
+
+	return d, err
 }
 
-func GetDeviceMapping(deviceUUID string) (int, string, error) {
+// Delete a map from the database
+// p is the outer bucket key (applicationUUID)
+// c is the inner bucket key (resinUUID)
+func Delete(p, c string) error {
 	db, err := open()
 	if err != nil {
-		return 0, "", err
+		return err
 	}
 	defer db.Close()
 
-	var applicationUUID []byte
-	var localUUID []byte
-	if err = db.View(func(tx *bolt.Tx) error {
-		var b *bolt.Bucket
-		if b = tx.Bucket([]byte("Mapping")); b == nil {
-			return fmt.Errorf("Bucket not found")
+	return db.Update(func(tx *bolt.Tx) error {
+		root, err := tx.CreateBucketIfNotExists([]byte("applications"))
+		if err != nil {
+			return err
 		}
 
-		var d *bolt.Bucket
-		if d = b.Bucket([]byte(deviceUUID)); d == nil {
-			return fmt.Errorf("Bucket not found")
+		parent, err := root.CreateBucketIfNotExists([]byte(p))
+		if err != nil {
+			return err
 		}
 
-		var value []byte
-		if value = d.Get([]byte("applicationUUID")); value == nil {
-			return fmt.Errorf("Value not found")
-		}
-
-		applicationUUID = make([]byte, len(value))
-		copy(applicationUUID, value)
-
-		if value = d.Get([]byte("localUUID")); value == nil {
-			return fmt.Errorf("Value not found")
-		}
-
-		localUUID = make([]byte, len(value))
-		copy(localUUID, value)
-
-		return nil
-	}); err != nil {
-		return 0, "", err
-	}
-
-	var a int
-	if a, err = b2i(applicationUUID); err != nil {
-		return 0, "", err
-	}
-
-	return a, (string)(localUUID), nil
+		return parent.DeleteBucket([]byte(c))
+	})
 }
 
-func init() {
-	log.SetLevel(config.GetLogLevel())
-
-	dir := config.GetDbDir()
-	name := config.GetDbName()
-	dbPath = path.Join(dir, name)
-
-	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
-		log.WithFields(log.Fields{
-			"Path":  dir,
-			"Error": err,
-		}).Fatal("Unable to create path")
-	}
-
+// Writes an array of maps to the database
+// p is the outer bucket key (applicationUUID)
+// cf is the inner bucket key field (resinUUID)
+// d is the array of key/value maps
+func Writes(p, cf string, d []map[string][]byte) error {
 	db, err := open()
 	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	return db.Update(func(tx *bolt.Tx) error {
+		root, err := tx.CreateBucketIfNotExists([]byte("applications"))
+		if err != nil {
+			return err
+		}
+
+		parent, err := root.CreateBucketIfNotExists([]byte(p))
+		if err != nil {
+			return err
+		}
+
+		for _, v := range d {
+			child, err := parent.CreateBucketIfNotExists(v[cf])
+			if err != nil {
+				return err
+			}
+
+			for ik, iv := range v {
+				if err := child.Put([]byte(ik), iv); err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	})
+}
+
+// WritesField writes a field to the child maps in the database
+// p is the outer bucket key (applicationUUID)
+// k is the field key
+// v is the field value
+func WritesField(p, k string, v []byte) error {
+	db, err := open()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	return db.Update(func(tx *bolt.Tx) error {
+		root, err := tx.CreateBucketIfNotExists([]byte("applications"))
+		if err != nil {
+			return err
+		}
+
+		parent, err := root.CreateBucketIfNotExists([]byte(p))
+		if err != nil {
+			return err
+		}
+
+		i := parent.Cursor()
+		for ik, _ := i.First(); ik != nil; ik, _ = i.Next() {
+			child := parent.Bucket(ik)
+
+			if err := child.Put([]byte(k), v); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+}
+
+// Reads an array of maps from the database
+// p is the outer bucket key (applicationUUID)
+// Returns array of maps key/value
+func Reads(p string) ([]map[string][]byte, error) {
+	db, err := open()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	var d []map[string][]byte
+
+	err = db.View(func(tx *bolt.Tx) error {
+		root := tx.Bucket([]byte("applications"))
+		if root == nil {
+			return err
+		}
+
+		parent := root.Bucket([]byte(p))
+		if parent == nil {
+			return fmt.Errorf("Bucket %s not found", (string)(p))
+		}
+
+		i := parent.Cursor()
+		for k, _ := i.First(); k != nil; k, _ = i.Next() {
+			child := parent.Bucket(k)
+			if child == nil {
+				return fmt.Errorf("Bucket %s not found", (string)(k))
+			}
+
+			m := make(map[string][]byte)
+
+			ii := child.Cursor()
+			for ik, iv := ii.First(); ik != nil; ik, iv = ii.Next() {
+				value := make([]byte, len(iv))
+				copy(value, iv)
+				m[(string)(ik)] = value
+			}
+
+			d = append(d, m)
+		}
+
+		return nil
+	})
+
+	return d, err
+}
+
+// ReadsField reads a field from the child maps in the database
+// p is the outer bucket key (applicationUUID)
+// k is the field key
+// Returns field map key/value
+func ReadsField(p, k string) (map[string][]byte, error) {
+	db, err := open()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	m := make(map[string][]byte)
+
+	err = db.View(func(tx *bolt.Tx) error {
+		root := tx.Bucket([]byte("applications"))
+		if root == nil {
+			return err
+		}
+
+		parent := root.Bucket([]byte(p))
+		if parent == nil {
+			return fmt.Errorf("Bucket %s not found", (string)(p))
+		}
+
+		i := parent.Cursor()
+		for ik, _ := i.First(); ik != nil; ik, _ = i.Next() {
+			child := parent.Bucket(ik)
+			if child == nil {
+				return fmt.Errorf("Bucket %s not found", (string)(ik))
+			}
+
+			v := child.Get([]byte(k))
+
+			value := make([]byte, len(v))
+			copy(value, v)
+			m[(string)(ik)] = value
+		}
+
+		return nil
+	})
+
+	return m, err
+}
+
+// Deletes maps from the database
+// p is the outer bucket key (applicationUUID)
+func Deletes(p string) error {
+	db, err := open()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	return db.Update(func(tx *bolt.Tx) error {
+		root, err := tx.CreateBucketIfNotExists([]byte("applications"))
+		if err != nil {
+			return err
+		}
+
+		return root.DeleteBucket([]byte(p))
+	})
+}
+
+func initialise(d, n string) {
+	if err := os.MkdirAll(d, os.ModePerm); err != nil {
 		log.WithFields(log.Fields{
 			"Path":  dbPath,
 			"Error": err,
-		}).Fatal("Unable to open database connection")
-	}
-	defer db.Close()
-
-	if err := makeBucket(db, "Applications"); err != nil {
-		log.WithFields(log.Fields{
-			"Error": err,
-		}).Fatal("Create bucket failed")
+		}).Fatal("Unable to create database path")
 	}
 
-	if err := makeBucket(db, "Mapping"); err != nil {
-		log.WithFields(log.Fields{
-			"Error": err,
-		}).Fatal("Create bucket failed")
-	}
-
-	log.WithFields(log.Fields{
-		"Path": dbPath,
-	}).Debug("Opened database connection")
+	dbPath = path.Join(d, n)
 }
 
 func open() (*bolt.DB, error) {
 	return bolt.Open(dbPath, 0600, nil)
 }
 
-func makeBucket(db *bolt.DB, name string) error {
-	return db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists([]byte(name))
-		return err
-	})
-}
+// func GetDeviceMapping(deviceUUID string) (int, string, error) {
+// 	db, err := open()
+// 	if err != nil {
+// 		return 0, "", err
+// 	}
+// 	defer db.Close()
 
-func putDeviceMapping(db *bolt.DB, applicationUUID int, localUUID, deviceUUID string) error {
-	return db.Update(func(tx *bolt.Tx) error {
-		var b *bolt.Bucket
-		if b = tx.Bucket([]byte("Mapping")); b == nil {
-			return fmt.Errorf("Bucket not found")
-		}
+// 	var applicationUUID []byte
+// 	var localUUID []byte
+// 	if err = db.View(func(tx *bolt.Tx) error {
+// 		var b *bolt.Bucket
+// 		if b = tx.Bucket([]byte("Mapping")); b == nil {
+// 			return fmt.Errorf("Bucket not found")
+// 		}
 
-		var d *bolt.Bucket
-		var err error
-		if d, err = b.CreateBucketIfNotExists([]byte(deviceUUID)); err != nil {
-			return err
-		}
+// 		var d *bolt.Bucket
+// 		if d = b.Bucket([]byte(deviceUUID)); d == nil {
+// 			return fmt.Errorf("Bucket not found")
+// 		}
 
-		var converted []byte
-		if converted, err = i2b(applicationUUID); err != nil {
-			return err
-		}
+// 		var value []byte
+// 		if value = d.Get([]byte("applicationUUID")); value == nil {
+// 			return fmt.Errorf("Value not found")
+// 		}
 
-		if err = d.Put([]byte("applicationUUID"), converted); err != nil {
-			return err
-		}
+// 		applicationUUID = make([]byte, len(value))
+// 		copy(applicationUUID, value)
 
-		return d.Put([]byte("localUUID"), []byte(localUUID))
-	})
-}
+// 		if value = d.Get([]byte("localUUID")); value == nil {
+// 			return fmt.Errorf("Value not found")
+// 		}
 
-func deleteDeviceMapping(db *bolt.DB, deviceUUID string) error {
-	return db.Update(func(tx *bolt.Tx) error {
-		var b *bolt.Bucket
-		if b = tx.Bucket([]byte("Mapping")); b == nil {
-			return fmt.Errorf("Bucket not found")
-		}
+// 		localUUID = make([]byte, len(value))
+// 		copy(localUUID, value)
 
-		return b.DeleteBucket([]byte(deviceUUID))
-	})
-}
+// 		return nil
+// 	}); err != nil {
+// 		return 0, "", err
+// 	}
 
-func i2b(value int) ([]byte, error) {
-	result := new(bytes.Buffer)
-	if err := binary.Write(result, binary.LittleEndian, (int32)(value)); err != nil {
-		return nil, err
-	}
+// 	var a int
+// 	if a, err = b2i(applicationUUID); err != nil {
+// 		return 0, "", err
+// 	}
 
-	return result.Bytes(), nil
-}
+// 	return a, (string)(localUUID), nil
+// }
 
-func b2i(value []byte) (int, error) {
-	var result int32
-	if err := binary.Read(bytes.NewReader(value), binary.LittleEndian, &result); err != nil {
-		return 0, err
-	}
+// func putDeviceMapping(db *bolt.DB, applicationUUID int, localUUID, deviceUUID string) error {
+// 	return db.Update(func(tx *bolt.Tx) error {
+// 		var b *bolt.Bucket
+// 		if b = tx.Bucket([]byte("Mapping")); b == nil {
+// 			return fmt.Errorf("Bucket not found")
+// 		}
 
-	return (int)(result), nil
-}
+// 		var d *bolt.Bucket
+// 		var err error
+// 		if d, err = b.CreateBucketIfNotExists([]byte(deviceUUID)); err != nil {
+// 			return err
+// 		}
+
+// 		var converted []byte
+// 		if converted, err = i2b(applicationUUID); err != nil {
+// 			return err
+// 		}
+
+// 		if err = d.Put([]byte("applicationUUID"), converted); err != nil {
+// 			return err
+// 		}
+
+// 		return d.Put([]byte("localUUID"), []byte(localUUID))
+// 	})
+// }
+
+// func deleteDeviceMapping(db *bolt.DB, deviceUUID string) error {
+// 	return db.Update(func(tx *bolt.Tx) error {
+// 		var b *bolt.Bucket
+// 		if b = tx.Bucket([]byte("Mapping")); b == nil {
+// 			return fmt.Errorf("Bucket not found")
+// 		}
+
+// 		return b.DeleteBucket([]byte(deviceUUID))
+// 	})
+// }
---
+++ database/database_test.go
@@ -0,0 +1,177 @@
+package database
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	"strconv"
+	"testing"
+)
+
+var M map[string][]byte
+var A []map[string][]byte
+
+func TestMain(m *testing.M) {
+	M = createMap(0)
+	A = createArray(10)
+
+	result := m.Run()
+
+	os.Exit(result)
+}
+
+func TestWriteRead(t *testing.T) {
+	setUp()
+
+	err := Write("parent", "child", M)
+	ok(t, err)
+
+	m, err := Read("parent", "child")
+	ok(t, err)
+	equals(t, M, m)
+
+	tearDown()
+}
+
+func TestWritesReads(t *testing.T) {
+	setUp()
+
+	err := Writes("parent", "key", A)
+	ok(t, err)
+
+	a, err := Reads("parent")
+	ok(t, err)
+	equals(t, A, a)
+
+	tearDown()
+}
+
+func TestWriteReads(t *testing.T) {
+	setUp()
+
+	err := Write("parent", "child", M)
+	ok(t, err)
+
+	m, err := Reads("parent")
+	ok(t, err)
+	equals(t, M, m[0])
+	equals(t, len(m), 1)
+
+	tearDown()
+}
+
+func TestWritesRead(t *testing.T) {
+	setUp()
+
+	err := Writes("parent", "key", A)
+	ok(t, err)
+
+	m, err := Read("parent", "0")
+	ok(t, err)
+	equals(t, A[0], m)
+
+	tearDown()
+}
+
+func TestWriteFieldReadField(t *testing.T) {
+	setUp()
+
+	err := Write("parent", "child", M)
+	ok(t, err)
+
+	err = WriteField("parent", "child", "key", []byte("changed"))
+	ok(t, err)
+
+	f, err := ReadField("parent", "child", "key")
+	ok(t, err)
+	equals(t, []byte("changed"), f)
+
+	tearDown()
+}
+
+func TestWritesFieldReadsField(t *testing.T) {
+	setUp()
+
+	err := Write("parent", "child1", M)
+	ok(t, err)
+	err = Write("parent", "child2", M)
+	ok(t, err)
+
+	err = WritesField("parent", "key", []byte("changed"))
+	ok(t, err)
+
+	f, err := ReadsField("parent", "key")
+	ok(t, err)
+	equals(t, f["child1"], []byte("changed"))
+	equals(t, f["child2"], []byte("changed"))
+
+	tearDown()
+}
+
+//WriteFieldReadsField
+//WritesFieldReadField
+//Delete
+//Deletes
+
+func createMap(key int) map[string][]byte {
+	type enum string
+	const (
+		test enum = "test"
+	)
+
+	m := make(map[string][]byte)
+
+	m["key"] = []byte(strconv.FormatInt((int64)(key), 10))
+	m["key1"] = []byte("value")                                //string
+	m["key2"] = []byte(strconv.FormatInt(1, 10))               //int
+	m["key3"] = []byte(strconv.FormatFloat(1.11, 'f', -1, 32)) //float32
+	m["key4"] = []byte(strconv.FormatFloat(1.11, 'f', -1, 64)) //float64
+	m["key5"] = []byte(strconv.FormatBool(true))               //bool
+	m["key6"] = []byte(test)                                   //type
+
+	return m
+}
+
+func createArray(n int) []map[string][]byte {
+	a := make([]map[string][]byte, 0, n)
+
+	for i := 0; i < n; i++ {
+		a = append(a, createMap(i))
+	}
+
+	return a
+}
+
+func setUp() {
+	initialise(".", "test.db")
+}
+
+func tearDown() {
+	os.Remove("test.db")
+}
+
+func ok(tb testing.TB, err error) {
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+		tb.FailNow()
+	}
+}
+
+func equals(tb testing.TB, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		tb.FailNow()
+	}
+}
+
+func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+	if !condition {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+		tb.FailNow()
+	}
+}
