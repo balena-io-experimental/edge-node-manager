3b38bb49b83fe5a314f828bc6431fde1ed5d22f0
Joseph Roberts <joe@resin.io>
2017-03-15 16:42:09 +0000

    Implement out of date warning
    Change-type: minor
    Signed-off-by: Joseph Roberts <joe@resin.io>

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"sort"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/jmoiron/jsonq"
 	"github.com/resin-io/edge-node-manager/api"
 	"github.com/resin-io/edge-node-manager/application"
 	"github.com/resin-io/edge-node-manager/config"
@@ -13,7 +15,13 @@ import (
 	"github.com/resin-io/edge-node-manager/supervisor"
 )
 
+var version string
+
 func main() {
+	if err := checkVersion(); err != nil {
+		log.Fatal("Unable to check version")
+	}
+
 	log.Info("Starting edge-node-manager")
 
 	supervisor.WaitUntilReady()
@@ -68,4 +76,34 @@ func init() {
 			}).Fatal("Unable to initialise incoming supervisor API")
 		}
 	}()
+}
+
+func checkVersion() error {
+	resp, err := http.Get("https://api.github.com/repos/resin-io/edge-node-manager/releases/latest")
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	data := map[string]interface{}{}
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return err
+	}
+
+	latest, err := jsonq.NewQuery(data).String("tag_name")
+	if err != nil {
+		return err
+	}
+
+	if version == latest {
+		return nil
+	}
+
+	log.WithFields(log.Fields{
+		"Current version": version,
+		"Latest version":  latest,
+		"Update command":  "git push resin master:resin-nocache",
+	}).Warn("Please update edge-node-manager")
+
+	return nil
 }
