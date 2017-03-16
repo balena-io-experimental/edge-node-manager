39da73067a3c674289db19b89299bb4a0e4695c5
Joseph Roberts <joe@resin.io>
2017-03-16 10:54:05 +0000

    Address PR comments
    Signed-off-by: Joseph Roberts <joe@resin.io>

--- release.sh
+++ release.sh
@@ -7,16 +7,11 @@ if [ -z "$ACCOUNT" ] || [ -z "$REPO" ] || [ -z "$ACCESS_TOKEN" ]; then
 	exit 1
 fi
 
-# Create a release
-rm -f request.json response.json
-cat > request.json <<-EOF
-{
-    "tag_name": "$TRAVIS_TAG",
-    "name": "$TRAVIS_TAG",
-    "body": "Release of version $TRAVIS_TAG.\n$1"
-}
-EOF
+json="{
+    \"tag_name\": \"$TRAVIS_TAG\",
+    \"name\": \"$TRAVIS_TAG\",
+    \"body\": \"Release of $TRAVIS_TAG.\n$1\"
+}"
 
-curl --data "@request.json" --header "Content-Type:application/json" \
-	"https://api.github.com/repos/$ACCOUNT/$REPO/releases?access_token=$ACCESS_TOKEN" \
-	-o response.json
+curl --data "$json" --header "Content-Type:application/json" \
+    "https://api.github.com/repos/$ACCOUNT/$REPO/releases?access_token=$ACCESS_TOKEN"
